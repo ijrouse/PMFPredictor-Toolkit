8280c64b20501cc2f9e140a6986a23e278378606
Ian Rouse <ian.rouse@ucd.ie>
2022-06-15 13:38:01 +0100

    Another attempt with large files removed

--- UpdateTemplates.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Calculate potentials but only for chemicals and surfaces which do not already possess them.
-python3 GenerateChemicalPotentials.py -f 0
-python3 GenerateSurfacePotentials.py -f 0
-#Rebuild the HGExpansions
-python3 HGExpandChemicalPotential.py
-python3 HGExpandSurfacePotential.py
